bffea7e8c9dce31f8ffc535fcdd331d07660c904
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-25 20:28:44 +0330

    Add functions to solve basic math operations

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+// The following functions calculate 4 basic mathematical operations
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function division(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
