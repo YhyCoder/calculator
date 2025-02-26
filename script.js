8203f145b71de2dae74841fd9b0ff2c36a7cda1d
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-26 10:40:56 +0330

    Add ability to display buttons & calculations

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-
 // The following functions calculate 4 basic mathematical operations
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -20,7 +16,7 @@ function division(firstNumber, secondNumber) {
 }
 
 // Solves mathematical operations and returns the result
-function operate(firstNumber, secondNumber, operator) {
+function calculate(firstNumber, secondNumber, operator) {
   switch (operator) {
     case "+":
       return add(firstNumber, secondNumber);
@@ -32,3 +28,74 @@ function operate(firstNumber, secondNumber, operator) {
       return division(firstNumber, secondNumber);
   }
 }
+
+function display() {
+  // Get DOM elements
+  const numberButtons = document.querySelectorAll(".number");
+  const operatorButtons = document.querySelectorAll(".operator");
+  const equalButton = document.querySelector(".equal");
+  const calculatorOperation = document.querySelector(".calculator__operation");
+  const calculatorResult = document.querySelector(".calculator__result");
+
+  // These variables store the values ​​of operations
+  let currentNumber = "";
+  let previousNumber = "";
+  let operator = "";
+  let finalAnswer = "";
+  let answerDisplayed = false;
+
+  numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", () => {
+      // After getting the answer, if the user clicks on any number, a new calculation will start
+      if (answerDisplayed === true) {
+        currentNumber = "";
+        answerDisplayed = false;
+        operator = "";
+        calculatorOperation.textContent = "";
+      }
+
+      // Click on any number and it will display it in the calculator display area
+      currentNumber += numberButton.textContent;
+      calculatorResult.textContent = currentNumber;
+    });
+  });
+
+  operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+      // Continuing the calculation on the answer
+      if (answerDisplayed === true) {
+        currentNumber = finalAnswer;
+        answerDisplayed = false;
+        operator = "";
+      }
+
+      // If the user enters a number, they can edit the calculation operator if needed
+      if (operator === "") {
+        previousNumber = currentNumber;
+        currentNumber = "";
+      }
+
+      // The operator cannot be used at the beginning of the calculation
+      if (previousNumber !== "") {
+        operator = operatorButton.textContent;
+        calculatorOperation.textContent = `${previousNumber} ${operator}`;
+      }
+    });
+  });
+
+  // Gets the answer and displays it
+  equalButton.addEventListener("click", () => {
+    // Completes the calculatorOperation part
+    calculatorOperation.textContent += ` ${currentNumber} ${equalButton.textContent}`;
+
+    finalAnswer = calculate(
+      Number(previousNumber),
+      Number(currentNumber),
+      operator
+    );
+    calculatorResult.textContent = finalAnswer;
+    answerDisplayed = true;
+  });
+}
+
+display();
